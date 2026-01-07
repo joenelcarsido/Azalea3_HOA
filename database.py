71712d74855169170dcde9874557c5a1b8807595
joenelcarsido <joenelcarsido03@gmail.com>
2026-01-08 01:40:53 +0800

    Update database.py

--- database.py
+++ database.py
@@ -3,21 +3,24 @@ import sqlite3
 DB_NAME = "hoa.db"
 
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+    return sqlite3.connect(DB_NAME, check_same_thread=False)
 
 def init_db():
     conn = get_connection()
     cur = conn.cursor()
 
+    # USERS
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
-        role TEXT NOT NULL
+        role TEXT NOT NULL,
+        disabled INTEGER DEFAULT 0
     )
     """)
 
+    # PAYMENTS
     cur.execute("""
     CREATE TABLE IF NOT EXISTS payments (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,4 +33,3 @@ def init_db():
 
     conn.commit()
     conn.close()
-
